e01f458b88fffd0e00e39d974be82c3d7b4dc183
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-11-02 12:12:13 +0530

    removing the update

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' https://js.stripe.com;
-              connect-src 'self' https://api.stripe.com https://js.stripe.com;
-              frame-src https://js.stripe.com https://hooks.stripe.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data:;
-            `.replace(/\s{2,}/g, " "),
-          },
-        ],
-      },
-    ];
-  },
-  experimental: {
-    scriptNonce: 'random', // Let Next.js safely allow inline scripts with a nonce
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
