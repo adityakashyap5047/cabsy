3c8ae85754102b2171b7085b56bd98a2b3a86d5a
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-11-18 18:52:12 +0530

    making the summary responsive

--- src/components/booking/AddDetails.tsx
+++ src/components/booking/AddDetails.tsx
@@ -156,7 +156,7 @@ export default function AddDetails({ isReturnJourney = false, forceMobileLayout 
   };
 
   const summary = (
-    <div className="grid grid-cols-2 gap-y-3">
+    <div className="grid grid-cols-2 gap-y-3 px-0 sm:px-4 md:px-24 lg:px-32">
       <div className="space-y-2 sm:space-y-3">
         <div className="flex gap-1 flex-wrap">
           <span className="font-semibold text-gray-700 text-sm sm:text-base">
